5c7c156abf8a0a34208cc2e74574715bf23dc8f7
Ikhtiary Rizky <ikhtiaryrizky@gmail.com>
2017-05-25 21:32:45 +0700

    My Django app, first commit

---
+++ python_intro.py
@@ -0,0 +1,49 @@
+#Fist Step (Hello World!)
+	# print("Hello Django, i'm coming!")
+
+#Second Step (Variable, if.. elif.. else)
+	# a = raw_input("Input A: ")
+	# b = raw_input("Input B: ")
+	# if a < b:
+	# 	print("B is more than A")
+	# else:
+	# 	print("A is more than B")
+	# nama = raw_input("Input a name: ")
+	# JK = raw_input("Input a gender: ")
+	# if JK == 'l':
+	# 	print ("Hi "+nama+" you are a Good boy!")
+	# elif JK == 'p':
+	# 	print ("Hi "+nama+" you are a Good girl!")
+	# else:
+	# 	print ("I don\'t know anything about you")
+
+#Third Step (Function)
+	# def hi(conditions):
+	# 	print "Me: Hi dude, how are u?"
+	# 	print "Ma friend: "+conditions
+	# 	# conditions = raw_input("Tell your conditions.. ")
+	# 	if conditions == "Good":
+	# 		print "Me: Hmm i glad you're Ok :D"
+	# 	elif conditions == "Not good":
+	# 		print "Me: I\m so sorry to heard that :("
+	# 	else:
+	# 		print "Me: Are u oke? :|"
+	# hi("Good")
+
+#Fourth Step (Looping)
+	# def hi(name):
+	# 	print "Hi "+ name+"!"
+	# friends = ['Tarjo','Cecep','Katro']
+	# for name in friends:
+	# 	hi(name)
+	# 	print "Nice to meet you.."		
+
+	# for a in range(1,20):
+	# 	if a % 15 == 0:
+	# 		print "Fizzbuz"
+	# 	elif a % 5 == 0:
+	# 		print "Buzz"
+	# 	elif a % 3 == 0:
+	# 		print "Fizz"
+	# 	else:
+	# 		print(a)
